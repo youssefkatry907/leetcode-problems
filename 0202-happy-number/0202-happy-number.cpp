85149e0b6bb68f8e093391311897b38bda2c2300
youssef Qatry <74834762+youssefkatry907@users.noreply.github.com>
2022-11-19 09:09:48 +0200

    Time: 3 ms (65.72%), Space: 6.1 MB (53.32%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
+    typedef long long ll;
+    map<ll, ll> mp;
     bool isHappy(int n) {
-        int sum = 0;
-     
-        while(n) {
-            int r = n % 10;
-            sum += pow(r,2);
-            n /= 10;
-        }
-     
-        if(sum == 1) return true; 
-    
-        else if(sum < 7) return false; 
+        bool ok = true;
         
-        else return isHappy(sum);
-
+    while (n != 1) {
+        n = getSum(n);
+        if(mp[n] > 1) {
+            ok = false;
+            break;
+        }
     }
+    return (ok ? true : false);
+    }
+    
+    ll fastpower(ll n, ll m) {
+    ll res = 1;
+    while (m > 0) {
+        if (m & 1)res = res * n;
+        n *= n;
+        m /= 2;
+    }
+    return res;
+}
+    
+    ll getSum(ll num) {
+    ll sum = 0;
+    while (num) {
+        sum += fastpower((num % 10), 2);
+        num /= 10;
+    }
+    mp[sum]++;
+    return sum;
+}
     
 };
